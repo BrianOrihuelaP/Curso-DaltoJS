c662c24327a54847081dd0e89e3b0c71d81bc579
Brian OrihuelaP <borihuelap001@alumno.uaemex.mx>
2022-07-09 21:37:10 -0500

    Subiendo el capitulo 4 - TERMINADO,  Metodos con Arrays  09/07/2022

--- chapter4/2.- Metodos con arreglos.js
+++ chapter4/2.- Metodos con arreglos.js
@@ -58,11 +58,11 @@ sonyGames.splice(1, 2);
 console.log("Vector resultante: " + sonyGames);
 
 sonyGames.splice(1, 2, "Little big planet", "Beyond two Souls");
-// vuelveme a eliminar de la posicion 1 al 2 y agregame estos elementos
+// vuelveme a eliminar de la posicion 1 al 2 y agregame estos elementos en su lugar
 console.log("Vector resultante: " + sonyGames);
 
 sonyGames.splice(1, 0, "Heavy Rain");
-// no me elimines nigun elemento y agrega el siguiente
+// no me elimines nigun elemento y agrega el siguiente despues del primero
 console.log("Vector resultante: " + sonyGames);
 
 sonyGames.splice(-1, 0, "Metal Gear Solid");
@@ -80,8 +80,8 @@ console.log("*******************************************JOIN()******************
 
 resultado = sonyGames.join();
 console.log(resultado + "  ->  Tipo de variable: " + typeof resultado);
-// join nos permite usar separadores de la siguiente manera: 
 
+// join nos permite usar separadores de la siguiente manera: 
 resultado = sonyGames.join("  -  ");
 // resultado = sonyGames.join("<br> elemento: "); 
 console.log(resultado + "  ->  Tipo de variable: " + typeof resultado);
@@ -108,6 +108,7 @@ sonyGames.push("God of war Ascencion");
 resultado = sonyGames.slice(3);
 console.log(resultado);
 // de esta otra manera se copian todos los elementos a partir de la posicion idicada
+// *NOTA = No tendria caso copiar todos los elementos con slice, simplemente igualamos y ya
 
 
 
@@ -140,6 +141,7 @@ sonyGames.filter(
         console.log("Juego de PS3: " + games);
     }
 );
+// *NOTA: Si se usa filter de la manera anterior no tendria caso dado que se usa como un simple for
 // Filtrando y usando el metodo de una manera mas apropiada--------------------------
 console.log("\n");
 let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -159,36 +161,79 @@ resultado = numeros.filter(
 console.log("Vector filtrado por impares: " + resultado);
 
 // Vector de objetos!
-
+console.log("VECTOR DE OBJETOS****************************************");
 const bestGames = [
     {
         name: "God of War 3",
-        release: "2013",
+        release: 2013,
         company: "SONY - Santa Monica",
     },
     {
         name: "The Last of Us",
-        release: "2013",
+        release: 2013,
         company: "SONY - Naughty Dog",
     },
     {
         name: "Resident Evil 4",
-        release: "2006",
-        company: "CampCom",
+        release: 2006,
+        company: "CapCom",
+    },
+    {
+        name: "Uncharted - Drake´s Fortune",
+        release: 2007,
+        company: "SONY - Naughty Dog",
     }
 ];
 
-resultado = bestGames.filter(
-    games =>{
-        games.name.includes("SONY")
-    }
-);
+console.log(bestGames);
+// imprimo el vector de objetos
 
-console.log("Array de objetos: " + bestGames[0]);
-console.log("Filtrado: " + resultado);
+resultado = bestGames.filter(games => games.release == 2006);
+// filtrame  los juegos los cuales salieron el 2006
+console.log("Filtrado: " + JSON.stringify(resultado));
+
+// *NOTA: LA FUNCION JSON.STR... NO PERMITE IMPRIMIR LO CONTENIDO EN UN ARRAY DE OBJETOS  
+
+resultado = bestGames.filter(games => games.company == "SONY - Naughty Dog");
+// filtrame solo los juegos hechos por naugthy dog
+console.log("FILTRADO: ");
+console.log(resultado);
+
+resultado = bestGames.filter(games => games.company == "SONY - Naughty Dog" && games.release == 2007);
+// filtrame solo los juegos hechos por naugthy dog y que salieron el 2007
+console.log("FILTRADO: ");
+console.log(resultado);
+// *NOTA: PARA PODER VER ADECUADAMENTE A UN ARRAY DE OBJETOS, NO DEBEMOS DE CONCATENAR TEXTO CON EL VECTOR
 
 //  *forEach() =  ejecuta la funcion indicada una vez por cada elemento del array 
 console.log("*******************************************FOREACH()**************************************************");
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let suma = 0;
+
+numbers.forEach(item => suma += item);
+// El foreach es muy similar al for, en este caso decimos: 
+// recorre cada elemento del array y lo vas a guardar en item, despues de al variable "suma" se le va a sumar el valor de cada elemento
+// en cada iteracion 
+console.log(suma);
+
+const letras = ['a', 'b', 'c', 'a', 'b', 'a', 'a', 'c', 'b'];
+let contador = {};
+// como usaremos vector de letras, ocuparemos un objeto para meter la cunta en su literal -> {"letra" : 2 } 
+
+letras.forEach(item => {
+    if (contador[item]) {
+        //recordar que: if(contador[item]) == if(contador[item] == true)
+        // si ya existe la letra en el objeto, sumamos en su literal una unidad
+        contador[item]++;
+    } else {
+        contador[item] = 1;
+        // si no existe la letra en el objeto, entonces agrega 1 ; EJEMPLO: {'a' : 1}
+    }
+}
+);
+console.log(contador);
 
 
 
